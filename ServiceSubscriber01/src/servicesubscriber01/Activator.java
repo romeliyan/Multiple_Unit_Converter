d5de517b91047ee8f929e24cda7c1231af1152f0
melan96 <it17006712@my.sliit.lk>
2019-03-29 14:07:18 +0530

    tracker implementation added to subscriber01 class

--- ServiceSubscriber01/src/servicesubscriber01/Activator.java
+++ ServiceSubscriber01/src/servicesubscriber01/Activator.java
@@ -1,22 +1,57 @@
 package servicesubscriber01;
 
+import datatransferrateprovider.DataRateService;
+import lengthprovider.Length;
+import massprovider.Mass;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+import temperatureprovider.Temperature;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	// private static BundleContext context;
 
-	static BundleContext getContext() {
-		return context;
-	}
+	private BundleContext context = null;
+	private ServiceTracker DRT_tracker = null;
+	private ServiceTracker TEMP_tracker = null;
+	private ServiceTracker MASS_tracker = null;
+	private ServiceTracker LENGTH_tracker = null;
+
+//	static BundleContext getContext() {
+//		return context;
+//	}
 
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+		// Activator.context = bundleContext;
+
+		context = bundleContext;
+
+		// get DRT_tracker Object
+		DRT_tracker = new ServiceTracker(context,
+				context.createFilter("(&(objectClass=" + DataRateService.class.getName() + ")" + "(Language=*))"),
+				null);
+		DRT_tracker.open();
+
+		// get TEMP_tracker Object
+		TEMP_tracker = new ServiceTracker(context,
+				context.createFilter("(&(objectClass=" + Temperature.class.getName() + ")" + "(Language=*))"), null);
+		DRT_tracker.open();
+
+		// get MASS_tracker Object
+		MASS_tracker = new ServiceTracker(context,
+				context.createFilter("(&(objectClass=" + Mass.class.getName() + ")" + "(Language=*))"), null);
+		DRT_tracker.open();
+
+		// get LENGTH_tracker Object
+		LENGTH_tracker = new ServiceTracker(context,
+				context.createFilter("(&(objectClass=" + Length.class.getName() + ")" + "(Language=*))"), null);
+		DRT_tracker.open();
+
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		// Activator.context = null;
 	}
 
 }
