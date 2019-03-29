1dcb30bb957ed261052fb3a866ea4dade582fd26
romeliyan <rkbliyanage@gmail.com>
2019-03-29 15:57:08 +0530

    change to context

--- MassProvider/src/massprovider/Activator.java
+++ MassProvider/src/massprovider/Activator.java
@@ -23,7 +23,7 @@ public class Activator implements BundleActivator {
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		Activator.context = bundleContext;
 		System.out.println("Mass provider stopped");
 		serviceRegistration.unregister();
 	}
