d214d25fe9fd145ee04150467c16c22f387fc795
melan96 <it17006712@my.sliit.lk>
2019-03-29 21:21:46 +0530

    Implementation for back to system 'working phase'

--- LengthProvider/src/lengthprovider/Activator.java
+++ LengthProvider/src/lengthprovider/Activator.java
@@ -1,31 +1,26 @@
 package lengthprovider;
 
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
-
-
-	private static BundleContext context;
-	private ServiceRegistration serviceRegistration;
-
-	static BundleContext getContext() {
-		return context;
-	}
+	
 
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-		System.out.println("Length provider started");
-		Length lengthService = new LengthImplementation();
-		serviceRegistration = context.registerService(Length.class.getName(),  lengthService, null);
-		
-	}
+		Hashtable<String, String> props = new Hashtable<String, String>();
+        props.put("Language", "English");
+        bundleContext.registerService(
+            Length.class.getName(), new LengthImplementation(), props);
+		System.out.println("Coffee service registered and started successfully");		
+    }
+	
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+//		Activator.context = bundleContext;
 		System.out.println("Length provider stopped");
-		serviceRegistration.unregister();
+//		serviceRegistration.unregister();
 	}
-
 }
--- ServiceSubscriber01/src/servicesubscriber01/Activator.java
+++ ServiceSubscriber01/src/servicesubscriber01/Activator.java
@@ -1,22 +1,110 @@
 package servicesubscriber01;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import lengthprovider.Length;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	// private static BundleContext context;
 
-	static BundleContext getContext() {
-		return context;
-	}
+	private BundleContext m_context = null;
+	// private ServiceTracker DRT_tracker = null;
+//	private ServiceTracker TEMP_tracker = null;
+//	private ServiceTracker MASS_tracker = null;
+	private ServiceTracker LENGTH_tracker = null;
+
+//	static BundleContext getContext() {
+//		return context;
+//	}
 
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+		// Activator.context = bundleContext;
+
+		// get DRT_tracker Object
+//		DRT_tracker = new ServiceTracker(context,
+//				context.createFilter("(&(objectClass=" + DataRateService.class.getName() + ")" + "(Language=*))"),
+//				null);
+//		DRT_tracker.open();
+//
+//		// get TEMP_tracker Object
+//		TEMP_tracker = new ServiceTracker(context,
+//				context.createFilter("(&(objectClass=" + Temperature.class.getName() + ")" + "(Language=*))"), null);
+//		DRT_tracker.open();
+//
+//		// get MASS_tracker Object
+//		MASS_tracker = new ServiceTracker(context,
+//				context.createFilter("(&(objectClass=" + Mass.class.getName() + ")" + "(Language=*))"), null);
+//		DRT_tracker.open();
+
+		// get LENGTH_tracker Object
+//		LENGTH_tracker = new ServiceTracker(context,
+//				context.createFilter("(&(objectClass=" + Length.class.getName() + ")" + "(Language=*))"), null);
+//		LENGTH_tracker.open();
+
+		m_context = bundleContext;
+
+		// Create a service tracker to monitor dictionary services.
+		LENGTH_tracker = new ServiceTracker(m_context,
+				m_context.createFilter("(&(objectClass=" + Length.class.getName() + ")" + "(Language=*))"), null);
+		LENGTH_tracker.open();
+
+		// User interaction panel
+		try {
+
+			System.out.println("------------------------------------------");
+			System.out.println("\n\n\n");
+			System.out.println("Select Number to Proceed");
+			System.out.println("01 - Data Rate Conversions");
+			System.out.println("02 - Length Conversions");
+			System.out.println("03 - Temperature Rate Conversions");
+			System.out.println("04 - Mass Conversions");
+			System.out.println("\n\n");
+			System.out.println("------------------------------------------");
+			System.out.println("Enter blank line to exit program");
+
+			String inputStream = "";
+			BufferedReader bufferedStream = new BufferedReader(new InputStreamReader(System.in));
+
+			// coffeeService coffee = (coffeeService) m_tracker.getService();
+			Length len = (Length) LENGTH_tracker.getService();
+			while (true) {
+
+				System.out.println("Select option Number : \n");
+				inputStream = bufferedStream.readLine();
+
+				if (inputStream.isEmpty()) {
+					System.out.println("Exiting......");
+					break;
+				} else if (inputStream.equals("1")) {
+					// your comment here ..
+					System.out.println("DTR - Login page ");
+					System.out.println(len.CMtoInches(455));
+
+				} else if (inputStream.equals("2")) {
+					// your comment here ..
+
+				} else if (inputStream.equals("3")) {
+					// your comment here ..
+
+				} else if (inputStream.equals("4")) {
+					// your comment here ..
+
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		// Activator.context = null;
 	}
 
 }
