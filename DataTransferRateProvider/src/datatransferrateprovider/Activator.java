072b8bdda247bad23f43327aed9252bf44bc852e
C03er <pasindu0996@gmail.com>
2019-03-29 13:40:31 +0530

    Activator Implementation

--- DataTransferRateProvider/src/datatransferrateprovider/Activator.java
+++ DataTransferRateProvider/src/datatransferrateprovider/Activator.java
@@ -5,11 +5,14 @@ import java.util.Hashtable;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 
 public class Activator implements BundleActivator {
 
+
 	private static BundleContext context;
+	private ServiceRegistration serviceRegistration;
 
 	static BundleContext getContext() {
 		return context;
@@ -17,16 +20,16 @@ public class Activator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		System.out.println("Data rate provider started");
+		DataRateService dataRateService = new DataRateImpl();
+		serviceRegistration = context.registerService(DataRateService.class.getName(),  dataRateService, null);
 		
-		Hashtable<String, String> props = new Hashtable<String, String>();
-        props.put("Language", "English");
-        context.registerService(
-        		DataRateService.class.getName(), new DataRateImpl(), props);
-		System.out.println("Dictionary service registered and started successfully");
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+		System.out.println("Data rate provider stopped");
+		serviceRegistration.unregister();
 	}
 
 }
