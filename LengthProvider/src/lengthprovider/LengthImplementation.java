c2ee57f7e37d86685b4933647b96f928b7910f9c
C03er <pasindu0996@gmail.com>
2019-03-29 13:27:16 +0530

    Length Activator Class Implementation

--- LengthProvider/src/lengthprovider/LengthImplementation.java
+++ LengthProvider/src/lengthprovider/LengthImplementation.java
@@ -1,5 +1,77 @@
 package lengthprovider;
 
-public class LengthImplementation {
+public class LengthImplementation implements Length{
+
+	@Override
+	public double KMtoMiles(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double MilestoKM(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double MetertoFeet(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double FeettoMeter(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double CMtoInches(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double InchestoCM(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double YardstoMeters(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double MeterstoYards(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double YardstoKM(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double KMtoYards(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double YardstoMiles(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double MilestoYards(double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
